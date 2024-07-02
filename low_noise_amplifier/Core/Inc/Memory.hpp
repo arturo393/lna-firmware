5769e250bfbd7c4b12950e51073460f3da627b67
arturo33 <arturo@sigma-telecom.com>
2024-07-02 08:38:11 -0400

    add memory private method

--- low_noise_amplifier/Core/Inc/Memory.hpp
+++ low_noise_amplifier/Core/Inc/Memory.hpp
@@ -22,14 +22,21 @@ public:
 
 class Memory {
 public:
-	Memory();
+	Memory(I2C_HandleTypeDef* _hi2c);
 	virtual ~Memory();
 	void createKey(std::string name,MemLocation address);
-
 	uint8_t* getValue(std::string name);
 	void setValue(std::string name,uint8_t value);
 private:
+	I2C_HandleTypeDef* hi2c;
+	uint8_t EEPROM_CHIP_ADDR = 0x50;
+	uint8_t EEPROM_PAGE_SIZE = 8;
+	uint8_t EEPrOM_PAGE_NUM = 32;
 	std::map<std::string, MemLocation> value_addr;
+	uint8_t EEPROM_Read(uint8_t address);
+	void EEPROM_Write(uint8_t address, uint8_t data);
+	uint16_t EEPROM_2byte_Write(uint8_t addr, uint16_t data);
+	void EEPROM_2byte_Read(uint8_t address);
 
 };
 
--- low_noise_amplifier/Core/Src/Memory.cpp
+++ low_noise_amplifier/Core/Src/Memory.cpp
@@ -10,7 +10,9 @@
 
 // Implementations of the member functions (would be placed outside the class)
 
-Memory::Memory() {}
+Memory::Memory(I2C_HandleTypeDef *_hi2c) {
+	hi2c = _hi2c;
+}
 
 Memory::~Memory() {}
 
@@ -24,4 +26,39 @@ uint8_t* Memory::getValue(std::string name) {
 
 void Memory::setValue(std::string name, uint8_t value) {
 
+}
+
+uint8_t Memory::EEPROM_Read(uint8_t address) {
+	uint8_t buff[2];
+	buff[0] = address;
+	HAL_I2C_Master_Transmit(hi2c, EEPROM_CHIP_ADDR << 1, buff, 1, 100);
+	HAL_I2C_Master_Receive(hi2c, EEPROM_CHIP_ADDR << 1 | 1, &buff[1], 1, 100);
+	return buff[1];
+}
+
+void Memory::EEPROM_Write(uint8_t address, uint8_t data) {
+	uint8_t buff[2];
+	uint8_t stored_data;
+	buff[0] = address;
+	buff[1] = data;
+
+	stored_data = EEPROM_Read(address);
+
+	if (stored_data != data)
+		HAL_I2C_Master_Transmit(hi2c, EEPROM_CHIP_ADDR << 1, buff, 2, 100);
+}
+
+void Memory::EEPROM_2byte_Write(uint8_t addr, uint16_t data) {
+	EEPROM_Write(addr, data & 0xff);
+	HAL_Delay(5);
+	EEPROM_Write(addr + 1, data >> 8);
+}
+
+uint16_t Memory::EEPROM_2byte_Read(uint8_t address) {
+	uint16_t data = 0;
+	data = EEPROM_Read(address + 1) << 8;
+	HAL_Delay(5);
+	data |= EEPROM_Read(address);
+
+	return data;
 }
