281444a931d96816b07217387292147389c242ec
Sebastian Torres <sebastian@uqomm.com>
2024-07-05 12:42:54 -0400

    Finished Command class

--- low_noise_amplifier/Core/Inc/Command.hpp
+++ low_noise_amplifier/Core/Inc/Command.hpp
@@ -9,98 +9,118 @@
 #include <cstring>
 #include <vector>
 // Abstract class Function
+
+
 class Command {
-
 public:
-	Command(uint8_t _module_function, uint8_t _module_id);
-	// Virtual destructor (optional)
-	virtual ~Command();
+    Command(uint8_t _module_function, uint8_t _module_id, uint8_t max_size);
+    Command(uint8_t _module_function, uint8_t _module_id); // Constructor que delega
+    virtual ~Command();
 
-	virtual void encode();
-	// Getters and Setters for private members
-	uint8_t getModuleFunction() {
-		return(module_function);
-	}
-	void setModuleFunction(uint8_t _module_function){
-		module_function = _module_function;
-	}
-	uint8_t getModuleId()  {
-		return(command_id);
-	}
-	void setModuleId(uint8_t _module_id){
-		module_id = _module_id;
-	}
-	uint8_t getCommandId()  {
-		return(module_function);
-	}
-	void setCommandId(uint8_t _command_id){
-		command_id = _command_id;
-	}
+    virtual void encode();
 
-	bool isListening() {
-		return (listening);
-	}
-	
-	uint8_t getLTELStartMark()  {
-		return (LTEL_START_MARK);
-	}
-	uint8_t getLTELEndMark() const {
-		return (LTEL_END_MARK);
-	}
-	uint8_t getMinFrameHeaderSize() const {
-		return (MIN_FRAME_HEADER_SIZE);
-	}
+    // Getters y Setters
+    uint8_t getModuleFunction() const {
+        return module_function;
+    }
+    void setModuleFunction(uint8_t _module_function) {
+        module_function = _module_function;
+    }
 
-	uint8_t getQueryParameterLTEL() const {
-		return (QUERY_PARAMETER_LTEL);
-	}
-	uint8_t getQueryParameterSigma() const {
-		return (QUERY_PARAMETER_SIGMA);
-	}
-	uint8_t getQueryParameterStr() const {
-		return (QUERY_PARAMETER_STR);
-	}
-	uint8_t getQueryADC() const {
-		return (QUERY_PARAMETER_ADC);
-	}
+    uint8_t getModuleId() const {
+        return module_id;
+    }
+    void setModuleId(uint8_t _module_id) {
+        module_id = _module_id;
+    }
 
-	uint8_t getSetAttLTEL() const {
-		return (SET_ATT_LTEL);
-	}
-	uint8_t getSetPoutMax() const {
-		return (SET_POUT_MAX);
-	}
-	uint8_t getSetPoutMin() const {
-		return (SET_POUT_MIN);
-	}
+    uint8_t getCommandId() const {
+        return command_id;
+    }
+    void setCommandId(uint8_t _command_id) {
+        command_id = _command_id;
+    }
+
+    bool isListening() const {
+        return listening;
+    }
+
+    bool isReady() const {
+        return ready;
+    }
+
+    void setMaxSize(uint8_t max_size) {
+        max_message_size = max_size;
+    }
+
+    uint8_t getMaxSize() const {
+        return max_message_size;
+    }
+
+    uint8_t getLTELStartMark() const {
+        return LTEL_START_MARK;
+    }
+    uint8_t getLTELEndMark() const {
+        return LTEL_END_MARK;
+    }
+    uint8_t getMinFrameHeaderSize() const {
+        return MIN_FRAME_HEADER_SIZE;
+    }
+
+    uint8_t getQueryParameterLTEL() const {
+        return QUERY_PARAMETER_LTEL;
+    }
+    uint8_t getQueryParameterSigma() const {
+        return QUERY_PARAMETER_SIGMA;
+    }
+    uint8_t getQueryParameterStr() const {
+        return QUERY_PARAMETER_STR;
+    }
+    uint8_t getQueryADC() const {
+        return QUERY_PARAMETER_ADC;
+    }
+
+    uint8_t getSetAttLTEL() const {
+        return SET_ATT_LTEL;
+    }
+    uint8_t getSetPoutMax() const {
+        return SET_POUT_MAX;
+    }
+    uint8_t getSetPoutMin() const {
+        return SET_POUT_MIN;
+    }
 
     void checkByte(uint8_t number);
 
 private:
+    void setVars();
+    void reset(bool init);
+    void reset();
+    bool prepareTxData(const char *message);
+    void handleRxData(uint8_t data);
+    bool validateChecksum();
+    bool checkCRC();
+    uint16_t calculateCRC(uint8_t start, uint8_t end);
+    std::vector<uint8_t> getData();
 
-	uint8_t max_message_size;
+    uint8_t max_message_size;
+    uint8_t module_function;
+    uint8_t module_id;
+    uint8_t command_id;
+    const uint8_t LTEL_START_MARK = 0x7e;
+    const uint8_t LTEL_END_MARK = 0x7f;
+    const uint8_t MIN_FRAME_HEADER_SIZE = 10;
 
-	uint8_t module_function;
-	uint8_t module_id;
-	uint8_t command_id;
-	uint8_t LTEL_START_MARK = 0x7e;
-	uint8_t LTEL_END_MARK = 0x7f;
-	uint8_t MIN_FRAME_HEADER_SIZE = 10;
+    const uint8_t QUERY_PARAMETER_LTEL = 0x11;
+    const uint8_t QUERY_PARAMETER_SIGMA = 0x12;
+    const uint8_t QUERY_PARAMETER_STR = 0x15;
+    const uint8_t QUERY_PARAMETER_ADC = 0x16;
 
-	uint8_t QUERY_PARAMETER_LTEL = 0x11;
-	uint8_t QUERY_PARAMETER_SIGMA = 0x12;
-	uint8_t QUERY_PARAMETER_STR = 0x15;
-	uint8_t QUERY_PARAMETER_ADC = 0x16;
+    const uint8_t SET_ATT_LTEL = 0x20;
+    const uint8_t SET_POUT_MAX = 0x24;
+    const uint8_t SET_POUT_MIN = 0x23;
 
-	uint8_t SET_ATT_LTEL = 0x20;
-	uint8_t SET_POUT_MAX = 0x24;
-	uint8_t SET_POUT_MIN = 0x23;
-
-	std::vector<uint8_t> message;
-	bool listening;
-
-	bool prepareTxData(const char *message);
-	void handleRxData(uint8_t data);
-	bool validateChecksum();
+    std::vector<uint8_t> message;
+    bool listening;
+    bool ready;
 };
-
--- low_noise_amplifier/Core/Src/Command.cpp
+++ low_noise_amplifier/Core/Src/Command.cpp
@@ -8,27 +8,67 @@
 
 #include "Command.hpp" // Include the header file
 
-Command::Command(uint8_t _module_function, uint8_t _module_id) {
-    module_function = _module_function;
-    module_id = _module_id;
-    max_message_size = 255; // Enforce max size
-    listening = false;
-    command_id = 0;
+Command::Command(uint8_t _module_function, uint8_t _module_id, uint8_t max_size)
+    : module_function(_module_function), module_id(_module_id), max_message_size(max_size) {
+    reset(true);
 }
 
-Command::~Command() {
+Command::Command(uint8_t _module_function, uint8_t _module_id)
+    : Command(_module_function, _module_id, 255) {}
 
-} // Empty destructor (optional)
+Command::~Command() {}
+
+void Command::setVars() {
+  #define MODULE_FUNCTION_BYTE 1
+  #define MODULE_ID_BYTE 2
+  #define COMMAND_BYTE 3
+
+  if (!ready) return;
+
+  command_id = message[COMMAND_BYTE];
+  module_id = message[MODULE_ID_BYTE];
+  module_function = message[MODULE_FUNCTION_BYTE];
+}
+
+std::vector<uint8_t> Command::getData(){
+  #define DATA_LENGTH_INDEX 5
+  #define DATA_INDEX 6
+
+  if (!ready) return std::vector<uint8_t>();
+
+  uint8_t length = message[5];
+  uint8_t end_index = DATA_INDEX + length;
+  return std::vector<uint8_t>(message.begin() + DATA_INDEX, message.begin() + end_index);
+}
+
+void Command::reset(bool init) {
+  if (!init) {
+  module_function = 0;
+  module_id = 0;
+  }
+
+  command_id = 0;
+  ready = false;
+  listening = false;
+  message.clear();
+}
+
+void Command::reset() {
+  reset(false);
+}
 
 void Command::checkByte(uint8_t number) {
   if (listening) {
     message.push_back(number);
     if (number == getLTELEndMark()) {
       listening = false;
+      ready = checkCRC();
+      if (ready) {
+        setVars();
+      }
     }
     if (message.size() >= max_message_size) {
-      message.clear();
-      listening = false;
+      reset()
     }
   } else {
     if (number == getLTELStartMark()) {
@@ -39,3 +79,38 @@ void Command::checkByte(uint8_t number) {
   }
 }
 
+bool Command::checkCRC() {
+  #define CRC_BYTE_1 3
+  #define CRC_BYTE_2 2
+
+
+	uint16_t crc;
+	uint8_t testframe[2];
+  uint8_t crc_frame[2] = { message[message.size() - CRC_BYTE_1], message[message.size() - CRC_BYTE_2]};
+
+	crc = calculateCRC(1, 3);
+	memcpy(testframe, &crc, 2);
+	if (testframe[1] == crc_frame[1] && testframe[0] == crc_frame[0]) {
+		return true;
+	}
+	return false;
+}
+
+uint16_t Command::calculateCRC(uint8_t start, uint8_t end) {
+	uint8_t b;
+	uint8_t i;
+	uint16_t generator = 0x1021; //divisor is 16bit
+	uint16_t crc = 0;			 // CRC value is 16bit
+  uint8_t end_byte = static_cast<uint8_t>(message.size()) - end;
+
+	for (b = start; 1 < end_byte; b++) {
+		crc ^= ((uint16_t) (message[b] << 8)); // move byte into MSB of 16bit CRC
+		for (i = 0; i < 8; i++) {
+			if ((crc & 0x8000) != 0) // test for MSB = bit 15
+				crc = ((uint16_t) ((crc << 1) ^ generator));
+			else
+				crc <<= 1;
+		}
+	}
+	return (crc);
+}
